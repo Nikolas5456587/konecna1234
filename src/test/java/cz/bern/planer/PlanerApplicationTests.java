cc2eb5c7ad917c80dab999372c170ef28e3ec651
Nikolas5456587 <Nikbernat10@gmail.com>
2024-06-24 17:48:33 +0200

    1st

---
+++ src/test/java/cz/bern/planer/PlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.bern.planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
